19ce04ac4a93e95b75d423d6485be44346ddb690
hljatgithub <126469777+hljatgithub@users.noreply.github.com>
2023-02-28 10:39:50 +0800

    Update main.py
    流程数据备注

--- main.py
+++ main.py
@@ -7,35 +7,38 @@ import warnings
 import argparse
 from cloths_segmentation.pre_trained_models import create_model
 
+# 当前路径项目HR-VITON内
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--background', type=bool, default=True, help='Define removing background or not')
     opt = parser.parse_args()
+
+    # Get mask of cloth
+    # 衣服原图和黑白蒙版图另存为
+    print("Get mask of cloth\n")
+    terminnal_command = "python get_cloth_mask.py" 
+    os.system(terminnal_command)
     
-    # Read input image
+    # Read input image # 模特原图
     original_path = input('模特图片路径：')
     img=cv2.imread(original_path)
     ori_img=cv2.resize(img,(768,1024))
     cv2.imwrite("./origin.jpg",ori_img)
 
-    # Resize input image
+    # Resize input image # 模特图重置大小给Graphonomy部位识别
     img=cv2.imread('origin.jpg')
     img=cv2.resize(img,(384,512))
     cv2.imwrite('resized_img.jpg',img)
 
-    # Get mask of cloth
-    print("Get mask of cloth\n")
-    terminnal_command = "python get_cloth_mask.py" 
-    os.system(terminnal_command)
-
     # Get openpose coordinate using posenet
-    # 生成关键点数据给HR-VITON训练预测
+    # 生成关键点数据00001_00_keypoints.json给Graphonomy部位识别
     print("Get openpose coordinate using posenet\n")
     terminnal_command = "python posenet.py" 
     os.system(terminnal_command)
 
     # Generate semantic segmentation using Graphonomy-Master library
-    # 人物部位识别、主体识别
+    # 人物部位识别、主体识别输出resized_segmentation_img
     print("Generate semantic segmentation using Graphonomy-Master library\n")
     os.chdir("./Graphonomy-master")
     terminnal_command ="python exp/inference/inference.py --loadmodel ./inference.pth --img_path ../resized_img.jpg --output_path ../ --output_name /resized_segmentation_img"
@@ -52,24 +55,24 @@ if __name__ == '__main__':
     img_seg=cv2.bitwise_and(ori_img,ori_img,mask=mask_img)
     back_ground=ori_img-img_seg
     img_seg=np.where(img_seg==0,215,img_seg)
-    # 主体无背景图
+    # 原图无背景图
     cv2.imwrite("./seg_img.png",img_seg)
     img=cv2.resize(img_seg,(768,1024))
     cv2.imwrite('./HR-VITON-main/test/test/image/00001_00.jpg',img)
 
     # Generate grayscale semantic segmentation image
-    # 灰度图
+    # 用主题图resized_segmentation_img制作灰度图
     terminnal_command ="python get_seg_grayscale.py"
     os.system(terminnal_command)
 
     # Generate Densepose image using detectron2 library
-    # detectron2可以做多种识别
+    # detectron2可以做多种识别,densepose姿态估计
     print("\nGenerate Densepose image using detectron2 library\n")
     terminnal_command ="python detectron2/projects/DensePose/apply_net.py dump detectron2/projects/DensePose/configs/densepose_rcnn_R_50_FPN_s1x.yaml \
     https://dl.fbaipublicfiles.com/densepose/densepose_rcnn_R_50_FPN_s1x/165712039/model_final_162be9.pkl \
     origin.jpg --output output.pkl -v"
     os.system(terminnal_command)
-    # 人体躯干
+    # 通过data.json姿态关键点数据保留人体躯干部分
     terminnal_command ="python get_densepose.py"
     os.system(terminnal_command)
 
