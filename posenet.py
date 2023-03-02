172ac6c4ca08fe499a715e4e7886efeb0e066be6
hljatgithub <126469777+hljatgithub@users.noreply.github.com>
2023-03-02 18:10:05 +0800

    Update posenet.py

--- posenet.py
+++ posenet.py
@@ -20,9 +20,9 @@ input_image, draw_image, output_scale = posenet.read_imgfile(testfile, scale_fac
 print(input_image.shape)
 with torch.no_grad():
     input_image = torch.Tensor(input_image).cuda() # 张量放到gpu上
-
+    # 输入特征根据网络结构进行处理
     heatmaps_result, offsets_result, displacement_fwd_result, displacement_bwd_result = net(input_image)
-
+    # 输出关键点数据
     pose_scores, keypoint_scores, keypoint_coords = posenet.decode_multiple_poses(
         heatmaps_result.squeeze(0),
         offsets_result.squeeze(0),
@@ -32,18 +32,20 @@ with torch.no_grad():
         max_pose_detections=20,
         min_pose_score=0.1)
 #print(pose_scores, len(pose_scores))
+
 poses = []
 # find face keypoints & detect face mask
 for pi in range(len(pose_scores)):
-    if pose_scores[pi] != 0.:
+    if pose_scores[pi] != 0.: # 可能识别出多批pose
         #print('Pose #%d, score = %f' % (pi, pose_scores[pi]))
         keypoints = keypoint_coords.astype(np.int32) # convert float to integer
         #print(keypoints[pi])
         poses.append(keypoints[pi])
+        
 # map rccpose-to-openpose mapping
 indices = [0, (5,6), 6, 8, 10, 5, 7, 9, 12, 14, 16, 11, 13, 15, 2, 1, 4, 3]
 i=0
-pose = poses[np.argmax(pose_scores)]
+pose = poses[np.argmax(pose_scores)] # 全部pose里取分数最高的
 openpose = []
 for ix in indices:
     if ix==(5,6):
@@ -63,7 +65,6 @@ pose_dic['pose_keypoints_2d'] = coords
 tmp = []
 tmp.append(pose_dic)
 data["people"]=tmp
-
 
 # VITON's .json is in ACGPN_TestData/test_pose/000001_0_keypoints.json
 pose_name = './HR-VITON-main/test/test/openpose_json/00001_00_keypoints.json' 
