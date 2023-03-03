597eaacb84aa52f9d6d8e918cf07ff85f520696b
hljatgithub <126469777+hljatgithub@users.noreply.github.com>
2023-03-03 19:53:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -16,12 +16,12 @@ if __name__ == '__main__':
 
     # Get mask of cloth
     # 衣服原图和黑白蒙版图另存为
-    print("Get mask of cloth\n")
+    print("1、获取服装mask图···············\n")
     terminnal_command = "python get_cloth_mask.py" 
     os.system(terminnal_command)
     
     # Read input image # 模特原图
-    original_path = input('模特图片路径：')
+    original_path = input('2、模特图片路径：')
     img=cv2.imread(original_path)
     ori_img=cv2.resize(img,(768,1024))
     cv2.imwrite("./origin.jpg",ori_img)
@@ -33,13 +33,13 @@ if __name__ == '__main__':
 
     # Get openpose coordinate using posenet
     # 生成关键点数据00001_00_keypoints.json
-    print("Get openpose coordinate using posenet\n")
+    print("3、posenet生成keypoints数据················\n")
     terminnal_command = "python posenet.py" 
     os.system(terminnal_command)
 
     # Generate semantic segmentation using Graphonomy-Master library
-    # 人物部位识别、主体识别输出resized_segmentation_img
-    print("Generate semantic segmentation using Graphonomy-Master library\n")
+    # 人物部位识别分割、主体识别输出resized_segmentation_img
+    print("4、基于Graphonomy-Master的DeepLabv3+模型生成segmentation图···············\n")
     os.chdir("./Graphonomy-master")
     terminnal_command ="python exp/inference/inference.py --loadmodel ./inference.pth --img_path ../resized_img.jpg --output_path ../ --output_name /resized_segmentation_img"
     os.system(terminnal_command)
@@ -47,9 +47,10 @@ if __name__ == '__main__':
 
     # Remove background image using semantic segmentation mask
     # 读取上面输出的主体图
+    print('5、通过segmentation图生成无背景图························\n')
     mask_img=cv2.imread('./resized_segmentation_img.png',cv2.IMREAD_GRAYSCALE)
     mask_img=cv2.resize(mask_img,(768,1024))
-    # 形态学转换~腐蚀erode，k为MORPH_RECT正方形核——白色区域变细
+    # 形态学转换~腐蚀erode，k为MORPH_RECT正方形核——有色区域变细，删掉更多背景
     k = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
     mask_img = cv2.erode(mask_img, k)
     # 原图与主体图and部分保留得到原图主体
@@ -64,12 +65,14 @@ if __name__ == '__main__':
 
     # Generate grayscale semantic segmentation image
     # 用主题图resized_segmentation_img制作灰度图
+    print('6、通过segmentation图生成灰度图························\n')
     terminnal_command ="python get_seg_grayscale.py"
     os.system(terminnal_command)
 
     # Generate Densepose image using detectron2 library
-    # detectron2可以做多种识别,densepose姿态估计
-    print("\nGenerate Densepose image using detectron2 library\n")
+    # detectron2可以做多种识别,densepose姿态估计输出data.json
+    print("7、通过detectron2生成densepose姿态估计························\n")
+    # 加载设置默认参数,生成data.json
     terminnal_command ="python detectron2/projects/DensePose/apply_net.py dump detectron2/projects/DensePose/configs/densepose_rcnn_R_50_FPN_s1x.yaml \
     https://dl.fbaipublicfiles.com/densepose/densepose_rcnn_R_50_FPN_s1x/165712039/model_final_162be9.pkl \
     origin.jpg --output output.pkl -v"
@@ -79,7 +82,7 @@ if __name__ == '__main__':
     os.system(terminnal_command)
 
     # Run HR-VITON to generate final image
-    print("\nRun HR-VITON to generate final image\n")
+    print("8、最终通过HR-VITON处理输出结果························\n")
     os.chdir("./HR-VITON-main")
     terminnal_command = "python3 test_generator.py --cuda True --test_name test1 --tocg_checkpoint mtviton.pth --gpu_ids 0 --gen_checkpoint gen.pth --datasetting unpaired --data_list t2.txt --dataroot ./test" 
     os.system(terminnal_command)
