151108de74985bfcb50b643a4d76fe75d5da82a3
hljatgithub <126469777+hljatgithub@users.noreply.github.com>
2023-03-09 17:53:19 +0800

    Update posenet.py

--- posenet.py
+++ posenet.py
@@ -4,6 +4,7 @@ from posenet.decode_multi import decode_multiple_poses
 from posenet.models.model_factory import load_model
 from posenet.utils import *
 import json
+import argparse
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
